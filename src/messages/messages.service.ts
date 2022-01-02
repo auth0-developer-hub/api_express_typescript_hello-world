538a088b6ea1e5bd792b0bb68261a6f976d6bd6e
dan <dan.arias@auth0.com>
2022-01-02 10:05:18 -0500

    improve message information

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -2,18 +2,24 @@ import { Message } from "../models/message";
 
 export const getPublicMessage = (): Message => {
   return {
-    text: "The API doesn't require an access token to share this message.",
+    api: "api_express_typescript_hello-world",
+    branch: "basic-authorization",
+    text: "The secured API doesn't require an access token to share this public message.",
   };
 };
 
 export const getProtectedMessage = (): Message => {
   return {
-    text: "The API successfully validated your access token.",
+    api: "api_express_typescript_hello-world",
+    branch: "basic-authorization",
+    text: "The secured API requires a valid access token to share this protected message.",
   };
 };
 
 export const getAdminMessage = (): Message => {
   return {
-    text: "The API successfully recognized you as an admin.",
+    api: "api_express_typescript_hello-world",
+    branch: "basic-authorization",
+    text: "The secured API requires a valid access token to share this admin message.",
   };
 };
